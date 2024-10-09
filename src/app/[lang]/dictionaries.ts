4dd0663031ece02c4c230f566085414b277271c3
ollierwoodman <ollierwoodman@gmail.com>
2024-10-10 01:31:48 +0800

    Fix errors

--- src/app/[lang]/dictionaries.ts
+++ src/app/[lang]/dictionaries.ts
@@ -1,5 +1,4 @@
 import 'server-only';
-<<<<<<< HEAD
 import { Dictionary } from '../../../types';
 
 const dictionaries: {
@@ -11,16 +10,3 @@ const dictionaries: {
 
 export const getDictionary = async (lang: string) => 
   dictionaries[lang]?.() ?? dictionaries.en();
-=======
-import { Locale } from '../../../i18n-config';
-
-const dictionaries: {
-  [key: string]: () => Promise<{} | Record<string, string>>
-} = {
-  en: () => import('./dictionaries/en.json').then((module) => module.default),
-  zh: () => import('./dictionaries/zh.json').then((module) => module.default),
-}
-
-export const getDictionary = async (locale: Locale) => 
-  dictionaries[locale]?.() ?? dictionaries.en();
->>>>>>> 68b24419416f0a5e1e0bb4990d5fb489e8cfe005
--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import Image from "next/image";
-=======
->>>>>>> 68b24419416f0a5e1e0bb4990d5fb489e8cfe005
 import { Social } from "../../types";
 import Button from "./Button";
 
@@ -43,18 +40,11 @@ export const SocialLink: React.FC<{
         getTwColorClassName(social.id)
       }`}
     >
-<<<<<<< HEAD
       <Image
         src={social.icon.src}
         alt={`${social.name} logo`}
         width={32}
         height={32}
-=======
-      <img
-        className="size-5 sm:h-8"
-        src={social.icon.src}
-        alt={`${social.name} logo`}
->>>>>>> 68b24419416f0a5e1e0bb4990d5fb489e8cfe005
       />
       <p className="text-sm font-bold sm:text-base">
         {social.text}
