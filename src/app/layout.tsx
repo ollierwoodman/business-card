3047e387f6da88664428e842009e7c0a1d8c9df5
ollierwoodman <75428439+ollierwoodman@users.noreply.github.com>
2024-10-11 01:34:35 +0000

    Add home redirect

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+export default function Root({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html>
+            <head>
+                <title>Ollie Woodman</title>
+            </head>
+            <body
+                className='bg-bg dark:bg-darkBg text-text dark:text-darkText min-h-screen'
+            >
+                {children}
+            </body>
+        </html>
+    )
+}
